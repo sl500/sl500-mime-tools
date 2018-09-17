a6a32a5162e3ac4a9e6f277e42af397b349d59b6
mbh wollmux <mbh_wollmux@rested.tvc.muenchen.de>
2018-09-17 08:38:48 +0200

    add globs (1) file to search. Firefox reacts to globs files if it comes to mime-types.

--- helper/searchForStringInBothGlobsFiles.sh
+++ helper/searchForStringInBothGlobsFiles.sh
@@ -10,3 +10,10 @@ grep -rni '/usr/share/mime/globs2' -e $searchString
 echo ""
 echo "searchForStringInGlobs2 END!"
 
+echo "searchForStringInGlobs1 BEGIN:"
+echo "search /usr/share/mime/globs:"
+echo ""
+grep -rni '/usr/share/mime/globs' -e $searchString
+echo ""
+echo "searchForStringInGlobs1 END!"
+
